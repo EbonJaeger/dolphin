93c70e9416628c282ec7cb3614f2d52dc0890d2b
Evan Maddock <maddock.evan@vivaldi.net>
2020-08-26 19:26:57 -0400

    Move config types into its own file
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- config/config.go
+++ config/config.go
@@ -11,38 +11,6 @@ import (
 	"github.com/pelletier/go-toml"
 )
 
-// RootConfig is our root config struct.
-type RootConfig struct {
-	Discord   DiscordConfig
-	Minecraft MinecraftConfig
-}
-
-// DiscordConfig holds all settings for the Discord side of the application.
-type DiscordConfig struct {
-	BotToken       string
-	ChannelID      string
-	AllowMentions  bool
-	UseMemberNicks bool
-	Webhook        WebhookConfig
-}
-
-// WebhookConfig holds settings for using Discord webhooks to send messages.
-type WebhookConfig struct {
-	Enabled bool
-	URL     string
-}
-
-// MinecraftConfig holds all settings for the Minecraft server side of the application.
-type MinecraftConfig struct {
-	RconIP              string
-	RconPort            int
-	RconPassword        string
-	TellrawTemplate     string
-	CustomDeathKeywords *[]string
-	UseLogFile          bool
-	LogFilePath         string
-}
-
 var configPath string
 
 // CreateConfigFile attempts to create the given config dir+file
